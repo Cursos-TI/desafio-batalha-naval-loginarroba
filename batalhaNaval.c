05843f4e6bdaf28017514484d8e5e8989017aaf2
loginarroba <login20atto@gmail.com>
2025-10-29 15:41:00 +0000

    BATALHA NOVATO

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,62 @@
 #include <stdio.h>
-
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+int main(){
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+        // iniciar variaveis , matirz e vetor(para definir as colunas visiveis no codigo)
+        int barco1 = 3;
+        int barco2 = 3;
+        int linha [10]={0,1,2,3,4,5,6,7,8,9,};
+        int tabuleiro[10][10]={
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0}
 
-    return 0;
-}
+        };
+             //definir a posicao dos barcos no tabuleiro
+             tabuleiro [9][7] = barco2;
+             tabuleiro [9][8] = barco2;
+             tabuleiro [9][9] = barco2;
+
+             tabuleiro [0][4] = barco1;
+             tabuleiro [1][4] = barco1;
+             tabuleiro [2][4] = barco1;
+
+            
+            //inicio do tabuleiro no terminal + linhas visiveis no terminal)
+            printf("    ----Tabuleiro----\n   ̲0̲ ̲1̲ ̲2̲ ̲3̲ ̲4̲ ̲5̲ ̲6̲ ̲7̲ ̲8̲ ̲9\n");
+             
+            
+
+        for (int i=0 ; i<10 ;i++){
+            printf("%d| " , linha[i]);
+            for (int j=0 ; j<10 ; j++){
+                
+                printf("%d " , tabuleiro[i][j]);
+            
+
+            }
+           
+            printf("|%d" , linha[i]);
+            printf("\n");
+            
+        }
+        printf("   ___________________\n   0 1 2 3 4 5 6 7 8 9\n");
+        //final do tabuleiro + linhas finais no terminal
+        
+        //localizando os barcos no tabuleiro
+        printf("\n");
+        printf("O primeiro barco esta localizado nas posicoes : (0,4) , (1,4) , (2,4)\n");
+        printf("O segundo barco esta localizado nas posicoes : (9,7) , (9,8) , (9,9)\n");
+
+
+
+
+      return 0;  
+    }
